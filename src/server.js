f1169f57f9f28073a7a87c5586fbe2eec8378f1e
arun-kumar-ak <arun1812.a@gmail.com>
2021-06-07 17:44:00 +0530

    nodemon added on heroku

--- src/server.js
+++ src/server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-// const dotenv = require('dotenv');
 
-// dotenv.config();
 const app = express();
 app.use(cors({
     credentials: true,
@@ -11,9 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 
 require('./config/database');
-// app.use('/',(req,res) => {
-//     res.send('its working on heroku')
-// });
+
 app.use('/',require('./api-routes/index'));
 
 const PORT = process.env.PORT || 3002;
