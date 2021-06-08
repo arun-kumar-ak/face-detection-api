c0ca652529deb458729dfa2200f49a29958d2dea
arun-kumar-ak <arun1812.a@gmail.com>
2021-06-08 12:21:27 +0530

    cors added on heroku

--- src/server.js
+++ src/server.js
@@ -3,7 +3,8 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors({
-    origin: ['http://localhost:3000/','http://localhost:3000/authUser','http://localhost:3000/signin','http://localhost:3000/register','http://localhost:3000/face-detect','http://localhost:3000/logout'],
+    // origin: ['http://localhost:3000','http://localhost:3000/authUser','http://localhost:3000/signin','http://localhost:3000/register','http://localhost:3000/face-detect','http://localhost:3000/logout'],
+    origin: "*",
     credentials: true,
 }));
 app.use(express.json());
