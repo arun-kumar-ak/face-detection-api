71d696e9cb0387e193997edb76392226636b93ce
arun-kumar-ak <arun1812.a@gmail.com>
2021-06-08 14:28:05 +0530

    cors added on heroku

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors({
-    origin: ["http://localhost:3000","https://arun-kumar-ak.github.io/face-detection"],
+    origin: ["http://localhost:3000","https://arun-kumar-ak.github.io/face-detection/"],
     credentials: true,
 }));
 app.use(express.json());
