64e5466ce6d72ed107bc27344891064ecb78c6e4
arun-kumar-ak <arun1812.a@gmail.com>
2021-06-08 11:13:20 +0530

    cors added on heroku

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors({
-    origin: "*",
+    origin: ['https://face-detection-api-fda.herokuapp.com/','https://face-detection-api-fda.herokuapp.com/authUser','https://face-detection-api-fda.herokuapp.com/signin','https://face-detection-api-fda.herokuapp.com/register','https://face-detection-api-fda.herokuapp.com/face-detect','https://face-detection-api-fda.herokuapp.com/logout'],
     credentials: true,
 }));
 app.use(express.json());
