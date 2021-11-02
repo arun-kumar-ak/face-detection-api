bbdffb64cd78e0b47ddbef7eab693f24f0f92ae4
arun-kumar-ak <arun1812.a@gmail.com>
2021-11-02 22:51:28 +0530

    url added

--- src/server.js
+++ src/server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
+app.use((req,res,next) => {
+    res.header('Access-Control-Allow-Origin',"*");
+})
 app.use(cors({
-    origin: ["http://localhost:3000","https://arun-kumar-ak.github.io","https://arun-kumar-ak.github.io/signin"],
     credentials: true,
 }));
 app.use(express.json());
