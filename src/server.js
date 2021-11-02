758ec7c59ea8e8b48a653c800ea146d11d7aa865
arun-kumar-ak <arun1812.a@gmail.com>
2021-11-02 22:52:44 +0530

    url added

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const app = express();
 app.use((req,res,next) => {
     res.header('Access-Control-Allow-Origin',"*");
+    next();
 })
 app.use(cors({
     credentials: true,
