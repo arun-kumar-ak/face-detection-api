4a2d9b5c81424062622bd086282d429dfb79468a
arun-kumar-ak <arun1812.a@gmail.com>
2021-11-11 19:39:58 +0530

    cors changed

--- src/server.js
+++ src/server.js
@@ -4,20 +4,6 @@ const cors = require('cors');
 const app = express();
 
 const origins = ["https://arun-kumar-ak.github.io","http://localhost:3000","http://192.168.56.1:3000","http://10.0.2.15:3000"];
-
-app.use(cors({
-    origin: (origin, callback) => {
-        console.log(origin)
-        if(origins.indexOf(origin) !== -1) {
-            console.log("accepted")
-            callback(null, true);
-        }
-    },
-    methods: "GET,POST",
-    preflightContinue: true,
-    credentials: true,
-    optionsSuccessStatus: 200,
-}));
 // app.use(function(req, res, next) {
 //     // res.header("Access-Control-Allow-Origin", "http://192.168.56.1:3000");
 //     res.header(
@@ -39,7 +25,19 @@ if(app.get('env') === 'development') {
 
 require('./config/database');
 
-app.use('/',require('./api-routes/index'));
+app.use('/',cors({
+    origin: (origin, callback) => {
+        console.log(origin)
+        if(origins.indexOf(origin) !== -1) {
+            console.log("accepted")
+            callback(null, true);
+        }
+    },
+    methods: "GET,POST",
+    preflightContinue: true,
+    credentials: true,
+    optionsSuccessStatus: 200,
+}),require('./api-routes/index'));
 
 const PORT = process.env.PORT || 3002;
 app.listen(PORT, ()=> {
