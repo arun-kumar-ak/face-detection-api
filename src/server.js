f8c66e21e89876b69c4f4cf045f148b303c3fd8b
arun-kumar-ak <arun1812.a@gmail.com>
2021-11-11 19:18:31 +0530

    origin func

--- src/server.js
+++ src/server.js
@@ -3,16 +3,28 @@ const cors = require('cors');
 
 const app = express();
 
-const origins = ["https://arun-kumar-ak.github.io","http://localhost:3000","http://192.168.56.1:3000"];
+const origins = ["https://arun-kumar-ak.github.io","http://localhost:3000","http://192.168.56.1:3000","http://10.0.2.15:3000"];
+
 app.use(cors({
     origin: (origin, callback) => {
-        if(true) {
+        console.log(origin)
+        if(origins.indexOf(origin) !== -1) {
+            console.log("accepted")
             callback(null, true);
         }
     },
+    // origin: origins,
     credentials: true,
     optionsSuccessStatus: 200,
 }));
+// app.use(function(req, res, next) {
+//     // res.header("Access-Control-Allow-Origin", "http://192.168.56.1:3000");
+//     res.header(
+//       "Access-Control-Allow-Headers",
+//       "Origin, X-Requested-With, Content-Type, Accept"
+//     );
+//     next();
+// });
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 
