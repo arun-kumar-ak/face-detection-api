8864f5ce06e469423c429a138fe2d27325a4828b
arun-kumar-ak <arun1812.a@gmail.com>
2021-11-11 20:14:08 +0530

    cors changed

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 
-console.log("url: "+process.env.MONGODB_URL);
-
 mongoose.connect(process.env.MONGODB_URL,{ useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }, (err) => {
     if(err) {
         console.log(err)
--- src/server.js
+++ src/server.js
@@ -1,31 +1,13 @@
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
+const timeout = require('connect-timeout');
+
 const app = express();
 
 const origins = ["https://arun-kumar-ak.github.io","http://localhost:3000","http://192.168.56.1:3000","http://10.0.2.15:3000"];
-// app.use(function(req, res, next) {
-//     // res.header("Access-Control-Allow-Origin", "http://192.168.56.1:3000");
-//     res.header(
-//       "Access-Control-Allow-Headers",
-//       "Origin, X-Requested-With, Content-Type, Accept"
-//     );
-//     next();
-// });
-app.use(express.json());
-app.use(express.urlencoded({extended: true}))
 
-if(app.get('env') !== 'development') {
-    app.set('trust proxy', 1)
-}
-
-if(app.get('env') === 'development') {
-    require('dotenv').config();
-}
-
-require(path.join(__dirname+'/config/database'));
-
-app.use('/',cors({
+app.use(timeout('100s'));
+app.use(cors({
     origin: (origin, callback) => {
         console.log(origin)
         if(origins.indexOf(origin) !== -1) {
@@ -37,7 +19,22 @@ app.use('/',cors({
     preflightContinue: true,
     credentials: true,
     optionsSuccessStatus: 200,
-}),require(path.join(__dirname+'/api-routes/index')));
+}));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
+if(app.get('env') !== 'development') {
+    app.set('trust proxy', 1)
+}
+
+if(app.get('env') === 'development') {
+    require('dotenv').config();
+}
+
+require('./config/database');
+
+app.use('/',require('./api-routes/index'));
 
 const PORT = process.env.PORT || 3002;
 app.listen(PORT, ()=> {
