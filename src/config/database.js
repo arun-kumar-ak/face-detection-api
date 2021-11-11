9ec696bca17eeaca4b1ff7a4116d86f66d4a1e0a
arun-kumar-ak <arun1812.a@gmail.com>
2021-11-11 19:55:00 +0530

    cors changed

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+console.log("url: "+process.env.MONGODB_URL);
+
 mongoose.connect(process.env.MONGODB_URL,{ useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }, (err) => {
     if(err) {
         console.log(err)
